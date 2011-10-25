66cab2fc7d47db81117483e0026153f5ca3fb152
vusile <vusile@gmail.com>
2011-10-25 06:07:33 +0300

    Changed Index.php to test git functions

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION DEVELOPMENT ENVIRONMENT 
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
